ce46f5b62e6c60042aafe01a0ff0daf28f1d3fcd
Alan Davies <alan@galax.xyz>
2021-02-02 21:28:09 -0500

    Entry map tests- now 100% coverage in containers module

---
+++ src/containers/charEntryMap_test.go
@@ -0,0 +1,94 @@
+package containers_test
+
+import (
+	"github.com/glxxyz/gohanzi/containers"
+	"testing"
+)
+
+func TestCharEntryMap_AddContains(t *testing.T) {
+	sut := containers.CharEntryMap{}
+	sut['A'] = EntryA
+	sut['B'] = EntryB
+	assert(t, sut.Contains('A'), "contains A")
+	assert(t, sut.Contains('B'), "contains B")
+	assert(t, !sut.Contains('C'), "doesn't contain C")
+	sut['C'] = EntryC
+	assert(t, sut.Contains('C'), "contains C")
+}
+
+func TestCharEntryMap_Difference(t *testing.T) {
+	AB := containers.CharEntryMap{}
+	AB['A'] = EntryA
+	AB['B'] = EntryB
+	BC := containers.CharEntryMap{}
+	BC['B'] = EntryB
+	BC['C'] = EntryC
+	result := AB.Difference(BC)
+	expected := containers.CharEntryMap{}
+	expected['A'] = EntryA
+	equals(t, expected, result)
+}
+
+func TestCharEntryMap_Intersection(t *testing.T) {
+	AB := containers.CharEntryMap{}
+	AB['A'] = EntryA
+	AB['B'] = EntryB
+	BC := containers.CharEntryMap{}
+	BC['B'] = EntryB
+	BC['C'] = EntryC
+	result := AB.Intersection(BC)
+	expected := containers.CharEntryMap{}
+	expected['B'] = EntryB
+	equals(t, expected, result)
+}
+
+func TestCharEntryMap_SubSetOf(t *testing.T) {
+	ABC := containers.CharEntryMap{}
+	ABC['A'] = EntryA
+	ABC['B'] = EntryB
+	ABC['C'] = EntryC
+	BC := containers.CharEntryMap{}
+	BC['B'] = EntryB
+	BC['C'] = EntryC
+	ABZ := containers.CharEntryMap{}
+	ABZ['A'] = EntryA
+	ABZ['B'] = EntryB
+	ABZ['Z'] = EntryZ
+	assert(t, ABC.SubsetOf(ABC), "ABC is a subset of ABC")
+	assert(t, BC.SubsetOf(ABC), "BC is a subset of ABC")
+	assert(t, !ABC.SubsetOf(BC), "ABC is not a subset of BC")
+	assert(t, !ABC.SubsetOf(ABZ), "ABC is not a subset of ABZ")
+}
+
+func TestCharEntryMap_Union(t *testing.T) {
+	AB := containers.CharEntryMap{}
+	AB['A'] = EntryA
+	AB['B'] = EntryB
+	BC := containers.CharEntryMap{}
+	BC['B'] = EntryB
+	BC['C'] = EntryC
+	result := AB.Union(BC)
+	equals(t, 2, len(AB))
+	equals(t, 2, len(BC))
+	expected := containers.CharEntryMap{}
+	expected['A'] = EntryA
+	expected['B'] = EntryB
+	expected['C'] = EntryC
+	equals(t, expected, result)
+}
+
+func TestCharEntryMap_AddAll(t *testing.T) {
+	sut := containers.CharEntryMap{}
+	sut['A'] = EntryA
+	sut['B'] = EntryB
+	BC := containers.CharEntryMap{}
+	BC['B'] = EntryB
+	BC['C'] = EntryC
+	sut.AddAll(BC)
+	expected := containers.CharEntryMap{}
+	expected['A'] = EntryA
+	expected['B'] = EntryB
+	expected['C'] = EntryC
+	equals(t, expected, sut)
+	equals(t, 2, len(BC))
+}
---
+++ src/containers/stringEntryMap_test.go
@@ -0,0 +1,94 @@
+package containers_test
+
+import (
+	"github.com/glxxyz/gohanzi/containers"
+	"testing"
+)
+
+func TestStringEntryMap_AddContains(t *testing.T) {
+	sut := containers.StringEntryMap{}
+	sut["A"] = EntryA
+	sut["B"] = EntryB
+	assert(t, sut.Contains("A"), "contains A")
+	assert(t, sut.Contains("B"), "contains B")
+	assert(t, !sut.Contains("C"), "doesn't contain C")
+	sut["C"] = EntryC
+	assert(t, sut.Contains("C"), "contains C")
+}
+
+func TestStringEntryMap_Difference(t *testing.T) {
+	AB := containers.StringEntryMap{}
+	AB["A"] = EntryA
+	AB["B"] = EntryB
+	BC := containers.StringEntryMap{}
+	BC["B"] = EntryB
+	BC["C"] = EntryC
+	result := AB.Difference(BC)
+	expected := containers.StringEntryMap{}
+	expected["A"] = EntryA
+	equals(t, expected, result)
+}
+
+func TestStringEntryMap_Intersection(t *testing.T) {
+	AB := containers.StringEntryMap{}
+	AB["A"] = EntryA
+	AB["B"] = EntryB
+	BC := containers.StringEntryMap{}
+	BC["B"] = EntryB
+	BC["C"] = EntryC
+	result := AB.Intersection(BC)
+	expected := containers.StringEntryMap{}
+	expected["B"] = EntryB
+	equals(t, expected, result)
+}
+
+func TestStringEntryMap_SubSetOf(t *testing.T) {
+	ABC := containers.StringEntryMap{}
+	ABC["A"] = EntryA
+	ABC["B"] = EntryB
+	ABC["C"] = EntryC
+	BC := containers.StringEntryMap{}
+	BC["B"] = EntryB
+	BC["C"] = EntryC
+	ABZ := containers.StringEntryMap{}
+	ABZ["A"] = EntryA
+	ABZ["B"] = EntryB
+	ABZ["Z"] = EntryZ
+	assert(t, ABC.SubsetOf(ABC), "ABC is a subset of ABC")
+	assert(t, BC.SubsetOf(ABC), "BC is a subset of ABC")
+	assert(t, !ABC.SubsetOf(BC), "ABC is not a subset of BC")
+	assert(t, !ABC.SubsetOf(ABZ), "ABC is not a subset of ABZ")
+}
+
+func TestStringEntryMap_Union(t *testing.T) {
+	AB := containers.StringEntryMap{}
+	AB["A"] = EntryA
+	AB["B"] = EntryB
+	BC := containers.StringEntryMap{}
+	BC["B"] = EntryB
+	BC["C"] = EntryC
+	result := AB.Union(BC)
+	equals(t, 2, len(AB))
+	equals(t, 2, len(BC))
+	expected := containers.StringEntryMap{}
+	expected["A"] = EntryA
+	expected["B"] = EntryB
+	expected["C"] = EntryC
+	equals(t, expected, result)
+}
+
+func TestStringEntryMap_AddAll(t *testing.T) {
+	sut := containers.StringEntryMap{}
+	sut["A"] = EntryA
+	sut["B"] = EntryB
+	BC := containers.StringEntryMap{}
+	BC["B"] = EntryB
+	BC["C"] = EntryC
+	sut.AddAll(BC)
+	expected := containers.StringEntryMap{}
+	expected["A"] = EntryA
+	expected["B"] = EntryB
+	expected["C"] = EntryC
+	equals(t, expected, sut)
+	equals(t, 2, len(BC))
+}
